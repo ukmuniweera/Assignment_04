df4dbafa46ae38a5a766b7d1ad298104152866c5
Udula Kesara Muniweera <153984452+ukmuniweera@users.noreply.github.com>
2024-11-29 13:24:32 +0530

    Project Finalized

--- animal-matching-game/src/App.js
+++ animal-matching-game/src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import GameBase from './components/GameBase';
 
 function App() {
--- animal-matching-game/src/components/GameBase.jsx
+++ animal-matching-game/src/components/GameBase.jsx
@@ -1,27 +1,45 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {animals} from '../data/AnimalsDb';
 
 export default function GameBase() {
-    const [animalName, setAnimalName] = useState('');
-    const matchAnimal=()=>{
-        if (animalName === 'deer') {
-            alert ('Won')
-        } else {
-            alert ('Lose')            
-        }
+    const [targetAnimal, setTargetAnimal] = useState('');
+    const [message, setMessage] = useState('');
+
+    useEffect(()=>{
+        setTargetAnimal(randomAnimal());
+    },[])
+
+    const randomAnimal=()=>{
+        const animalID = Math.floor(Math.random() * animals.length) + 1;
+        return (animals[animalID].name);
     }
+
+    const handleAnimalClick=(selectedAnimal)=>{
+        if (selectedAnimal === targetAnimal) {
+            setMessage('You Won!');
+        } else {
+            setMessage('You Lost!');
+        }
+        setTimeout(()=>{
+            setTargetAnimal(randomAnimal());
+            setMessage('');
+        },2000);
+    }
+    
   return (
     <div>
+        <h1>Animal Matching Game</h1>
+        <h2>Find: {targetAnimal}</h2>
+        {message && <h3>{message}</h3>}
         {animals.map((animal)=>(
             <button>
                 <img 
-                    src={require('../assets/fig/'+animal.img)} 
-                    alt={animal.name} 
-                    onClick={()=>setAnimalName(animal.name)}
+                src={require('../assets/fig/'+animal.img)} 
+                alt={animal.name} 
+                onClick={()=>{handleAnimalClick(animal.name)}}
                 />
             </button>
-        ))} 
-        <p onPlay={matchAnimal}></p>
+        ))}
     </div>
   );
 }
