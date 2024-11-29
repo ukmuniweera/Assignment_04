5d361d32e8ca70b672f1652421b7f329a0a07e20
Udula Kesara Muniweera <153984452+ukmuniweera@users.noreply.github.com>
2024-11-29 13:40:13 +0530

    Bug fixed

--- animal-matching-game/src/components/GameBase.jsx
+++ animal-matching-game/src/components/GameBase.jsx
@@ -1,45 +1,43 @@
-import { useEffect, useState } from 'react';
-import {animals} from '../data/AnimalsDb';
+import React, { useState, useEffect } from 'react';
+import { animals } from '../data/AnimalsDb';
 
 export default function GameBase() {
-    const [targetAnimal, setTargetAnimal] = useState('');
-    const [message, setMessage] = useState('');
+  const [targetAnimal, setTargetAnimal] = useState('');
+  const [message, setMessage] = useState('');
 
-    useEffect(()=>{
-        setTargetAnimal(randomAnimal());
-    },[])
+  useEffect(() => {
+    setTargetAnimal(randomAnimal());
+  }, []);
 
-    const randomAnimal=()=>{
-        const animalID = Math.floor(Math.random() * animals.length) + 1;
-        return (animals[animalID].name);
+  const randomAnimal = () => {
+    const animalID = Math.floor(Math.random() * animals.length);
+    return animals[animalID].name;
+  };
+
+  const handleAnimalClick = (selectedAnimal) => {
+    if (selectedAnimal === targetAnimal) {
+      setMessage('You Won!');
+    } else {
+      setMessage('You Lost!');
     }
+    setTimeout(() => {
+      setTargetAnimal(randomAnimal());
+      setMessage('');
+    }, 2000);
+  };
 
-    const handleAnimalClick=(selectedAnimal)=>{
-        if (selectedAnimal === targetAnimal) {
-            setMessage('You Won!');
-        } else {
-            setMessage('You Lost!');
-        }
-        setTimeout(()=>{
-            setTargetAnimal(randomAnimal());
-            setMessage('');
-        },2000);
-    }
-    
   return (
     <div>
-        <h1>Animal Matching Game</h1>
-        <h2>Find: {targetAnimal}</h2>
-        {message && <h3>{message}</h3>}
-        {animals.map((animal)=>(
-            <button>
-                <img 
-                src={require('../assets/fig/'+animal.img)} 
-                alt={animal.name} 
-                onClick={()=>{handleAnimalClick(animal.name)}}
-                />
-            </button>
+      <h1>Animal Matching Game</h1>
+      <h2>Find: {targetAnimal}</h2>
+      {message && <h3>{message}</h3>}
+      <div>
+        {animals.map((animal) => (
+          <button key={animal.name} onClick={() => handleAnimalClick(animal.name)}>
+            <img src={require(`../assets/fig/${animal.img}`)} alt={animal.name} />
+          </button>
         ))}
+      </div>
     </div>
   );
 }
